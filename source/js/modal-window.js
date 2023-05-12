b0ec650fe8d5fd78f4becc6549f9434dceef54ab
DepreKotik <vovachegotov@gmail.com>
2023-05-12 17:18:54 +0300

    Подготовка к защите

--- source/js/modal-window.js
+++ source/js/modal-window.js
@@ -1,11 +1,10 @@
-let modalWindow = document.querySelector('.modal-window');
-let openButton = document.querySelector('.modal-button');
-let closeButton = document.querySelector('.modal-window__close-button');
+const modalWindow = document.querySelector('.modal-window');
+const openButton = document.querySelector('.modal-button');
 
-openButton.addEventListener('click', function () {
-    modalWindow.classList.add('modal-window--opened');
-  });
+openButton.addEventListener('click', () => modalWindow.classList.add('modal-window--opened'));
 
-closeButton.addEventListener('click', function () {
-  modalWindow.classList.remove('modal-window--opened');
+modalWindow.addEventListener('click', (evt) => {
+  if (!evt.target.closest('.modal-window__container')) {
+    modalWindow.classList.remove('modal-window--opened');
+  }
 });
